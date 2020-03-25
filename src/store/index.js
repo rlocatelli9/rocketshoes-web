d55c02098e53107c5994d0b356d2e695d468ea3a
Robson Locatelli <rlocatelli9@gmail.com>
2020-03-25 13:55:43 -0300

    Revert "decouple actions"
    This reverts commit 0c15dcdb3392f0e04fbc68e05d32dcfd03a5cd11.

--- src/store/index.js
+++ src/store/index.js
@@ -4,14 +4,7 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
-const sagaMonitor =
-  process.env.NODE_ENV === 'development'
-    ? console.tron.createSagaMonitor()
-    : null;
-
-const sagaMiddleware = createSagaMiddleware({
-  sagaMonitor,
-});
+const sagaMiddleware = createSagaMiddleware();
 
 const enhancer =
   process.env.NODE_ENV === 'development'
